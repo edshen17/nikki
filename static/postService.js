7cadd439bdff776963b2358680f9fdfd1058575a
Edwin Shen <edshen17@avenues.org>
2019-06-20 10:11:49 -0400

    fixed like buttons not changing colors

--- static/postService.js
+++ static/postService.js
@@ -5,10 +5,11 @@ Vue.component('posts', {
     template: `
         <div class='blog-post py-2'>
             <h3> {{post.title}} </h3>
+            <p> {{post.likedBy}}</p>
             <h6> {{post.postedBy}} </h6>
             <div v-html='post.content'></div>
             <span class='likes'>
-            <i class='far fa-heart py-2' v-on:click="$emit('like')" v-bind:class='{far: !liked, fas: liked, colorRed: liked}'></i>
+            <i class='far fa-heart py-2' v-on:click='likePost(post)' v-bind:class='{far: !post.liked, fas: post.liked, colorRed: post.liked}'></i>
             {{post.likedBy.length}} 
             </span>
             <span class='comments'>
@@ -16,18 +17,23 @@ Vue.component('posts', {
             {{post.comments.length}}
             </span>
         </div>
-    `
+    `,
+    methods: {
+        likePost: function(post) {
+            this.$emit('like-post', this.post)
+        }
+    }
 });
       
 const postComponent = new Vue({
     el: '#posts',
     data: {
-        posts: null,
-        liked: false
+        posts: null
     }, 
     methods: {
-        like: function() {
-            this.liked = !this.liked
+        like: function(post) {
+            post.liked = !post.liked
+            post.likedBy.push('test')
         }
     },
     mounted () {
