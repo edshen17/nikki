0bc2525445d231c7395fb9b6fd6492b6cfd341a8
edshen17 <shen.ed@husky.neu.edu>
2019-06-29 15:54:33 -0400

    installed eslint and modified code to follow the airbnb style guide

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,30 @@
-'use strict'
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
 // Passes user variable from passport session to all pages,
 // allowing the navbar to show different links if user is logged in or not
-router.get('*', function(req, res, next) {
+router.get('*', (req, res, next) => {
   res.locals.user = req.user || null;
   next();
 });
 
 // GET /
-router.get('/', function(req, res) {
-  const title = 'Home'
+router.get('/', (req, res) => {
+  const title = 'Home';
   return res.render('index', {
-    title
+    title,
   });
-  
 });
 
 // GET /dashboard
-router.get('/dashboard', ensureAuthenticated, function(req, res) {
-  const title= `${req.user.username} | Dashboard`;
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+  const title = `${req.user.username} | Dashboard`;
   const username = req.user.username;
-  
   return res.render('dashboard', {
     title,
-    username
+    username,
   });
-
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,114 +1,115 @@
-'use strict'
 const express = require('express');
+
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
-const Post = require('../models/Post').Post;
-const Comment = require('../models/Post').Comment;
 const passport = require('passport');
+const User = require('../models/User');
+const { Post } = require('../models/Post');
+const { Comment } = require('../models/Post');
 const { ensureAuthenticated } = require('../config/auth');
-const ObjectId = require('mongoose').Types.ObjectId; 
+const { ObjectId } = require('mongoose').Types;
+
 
 // Accesses the id param and searches posts by id
-router.param('id', function(req, res, next, id) {
-  Post.findById(id, function(err, post) {
+router.param('id', (req, res, next, id) => {
+  Post.findById(id, (err, post) => {
     if (err) return next(err);
     if (!post) {
-      err = new Error('Not Found');
-      err.status = 404;
-      return next(err);
+      let error = err;
+      error = new Error('Not Found');
+      error.status = 404;
+      return next(error);
     }
     req.post = post;
     return next();
   });
 });
 
-//GET /login
-router.get('/login', function(req, res, next) {
+// GET /login
+router.get('/login', (req, res) => {
   const title = 'Login';
   const error = req.flash('error');
 
   return res.render('login', {
     title,
-    error
+    error,
   });
 });
 
 // GET users/register
-router.get('/register', function(req, res, next) {
+router.get('/register', (req, res) => {
   const title = 'Register';
   return res.render('register', {
-    title
+    title,
   });
 });
 
 // POST /users/register
 // Making a user in the db
-router.post('/register', function(req, res, next) {
-  const { username, email, password, password2 } = req.body;
-  let errors = [];
+router.post('/register', (req, res) => {
+  const {
+    username, email, password, password2,
+  } = req.body;
+  const errors = [];
 
   // Check if user did not fill out all inputs
   if (!username || !email || !password || !password2) {
-    errors.push({msg: 'Please fill out all fields'})
+    errors.push({ msg: 'Please fill out all fields' });
   }
 
   // Check passwords
   if (password !== password2) {
-    errors.push({msg: 'Passwords do not match!'});
+    errors.push({ msg: 'Passwords do not match!' });
   }
 
   if (password.length < 6) {
-    errors.push ({msg: 'Password should be at least 6 characters long'});
+    errors.push({ msg: 'Password should be at least 6 characters long' });
   }
 
-  if(errors.length > 0) {
+  if (errors.length > 0) {
     res.render('register', {
       errors,
       username,
       email,
       password,
-      password2
+      password2,
     });
-  }
-
-  else {
+  } else {
     // Validation
-    User.findOne({ email: email})
-      .then(user => {
+    User.findOne({ email })
+      .then((user) => {
         if (user) {
           // user exists
-            errors.push({msg: 'A user with that email already exists'});
-            res.render('register', {
-              errors,
-              username,
-              email,
-              password,
-              password2
-            });
+          errors.push({ msg: 'A user with that email already exists' });
+          res.render('register', {
+            errors,
+            username,
+            email,
+            password,
+            password2,
+          });
         } else {
           const newUser = new User({
-            username: username,
-            email: email,
-            password: password
+            username,
+            email,
+            password,
           });
 
           // Hash password
-          bcrypt.genSalt(10, (err, salt) =>
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if (err) throw err;
-              newUser.password = hash;
-              newUser.save()
-                .then(user => {
-                  req.login(user, function(err) {
-                    if (err) {
-                      console.log(err);
-                    } else {
-                      return res.redirect('/dashboard');
-                    }
-                  })
-                })
-                .catch(err => console.log(err));
+          bcrypt.genSalt(10, (err, salt) => bcrypt.hash(newUser.password, salt, (hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(() => {
+                req.login(user, (err) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    return res.redirect('/dashboard');
+                  }
+                });
+              })
+              .catch(console.log(err));
           }));
         }
       });
@@ -120,69 +121,63 @@ router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/dashboard',
     failureRedirect: '/users/login',
-    failureFlash: true
+    failureFlash: true,
   })(req, res, next);
 });
 
 // GET /users/logout
-router.get('/logout', (req, res, next) => {
+router.get('/logout', (req, res) => {
   req.logout();
-  res.status(301).redirect('/users/login')
+  res.status(301).redirect('/users/login');
 });
 
 // GET /users/:username
-// Route for getting a specific user's profile 
-router.get('/:username', function(req, res, next) {
-  User.find({username: req.params.username}, 'bio username imageURL')
-      .exec(function(err, users) {
-       
-        if (err) return next(err); 
-        if (users.length === 0) {
-          err = new Error('User does not exist');
-          err.status = 404;
-          return next(err);
-        }
+// Route for getting a specific user's profile
+router.get('/:username', (req, res, next) => {
+  User.find({ username: req.params.username }, 'bio username imageURL')
+    .exec((err, users) => {
+      if (err) return next(err);
+      if (users.length === 0) {
+        const error = new Error('User does not exist');
+        error.status = 404;
+        return next(error);
+      }
+      let loggedUser = null;
+      const bio = users[0].bio;
+      const username  = req.params.username;
 
-        else {
-          let loggedUser = null;
-          const bio = users[0].bio;
-          let username = req.params.username;
-
-          if (req.user) { // if user is logged in, use session var to create a client-side user object (omitting email/password)
-              
-            const imageURL = req.user.imageURL;
-            const bio = req.user.bio;
-            const _id = req.user.id;
-            const username = req.user.username;
-            loggedUser = {
-              imageURL,
-              bio,
-              _id,
-              username 
-            }
-          } 
-      
-          return res.render('profile', {username, bio, loggedUser});
-        }
-      });
+      if (req.user) { // if user is logged in, use session var to create a client-side user object (omitting email/password)           
+        const imageURL = req.user.imageURL;
+        const bio = req.user.bio;
+        const _id = req.user.id;
+        const username = req.user.username;
+        loggedUser = {
+          imageURL,
+          bio,
+          _id,
+          username,
+        };
+      }
+      return res.render('profile', { username, bio, loggedUser });
+    });
 });
 
 // GET /users/:username/json
 // Route for getting a specific user's json data
-router.get('/:username/json', function(req, res, next){
-  User.find({username: req.params.username}, 'bio username imageURL')
-      .exec(function(err, user) {
-        if (err) return next(err);
-        return res.status(200).json(user);
-      });
+router.get('/:username/json', (req, res, next) => {
+  User.find({ username: req.params.username }, 'bio username imageURL')
+    .exec((err, user) => {
+      if (err) return next(err);
+      return res.status(200).json(user);
+    });
 });
 
 // GET /users/:username/posts
-// Route for getting all the posts of a user 
-router.get('/:username/posts', function(req, res, next) {
-  Post.find({postedBy: req.params.username})
-    .sort({createdAt: -1})
-    .exec(function(err, posts) {
+// Route for getting all the posts of a user
+router.get('/:username/posts', (req, res, next) => {
+  Post.find({ postedBy: req.params.username })
+    .sort({ createdAt: -1 })
+    .exec((err, posts) => {
       if (err) return next(err);
       res.status(200).json(posts);
     });
@@ -191,19 +186,19 @@ router.get('/:username/posts', function(req, res, next) {
 
 // GET /users/:username/posts/:id
 // Route for getting a specific post
-router.get('/:username/posts/:id', function(req, res, next) {
+router.get('/:username/posts/:id', (req, res, next) => {
   res.send(req.post);
 });
 
 // POST /users/:username/posts
 // Route for creating a post
-router.post('/:username/posts', function(req, res, next) {
+router.post('/:username/posts', (req, res, next) => {
   const postedBy = req.params.username;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({ postedBy, title, content });
 
-  post.save(function(err, post) {
+  post.save((err, post) => {
     if (err) return next(err);
     res.status(201).send(post);
   });
@@ -211,38 +206,36 @@ router.post('/:username/posts', function(req, res, next) {
 
 // POST /users/:username/posts
 // Route for liking/unliking a post
-router.post('/:username/posts/:id/like', function(req, res, next) {
-  Post.findById(req.params.id, function(err, post) {
+router.post('/:username/posts/:id/like', (req, res, next) => {
+  Post.findById(req.params.id, (err, post) => {
     if (err) return next(err);
-    else {
-      post.likedBy = req.body.likedBy;
-      post.save(function(err, post) {
-        if (err) return next(err);
-        res.status(200).send(post);
-      });
-    }
-  });  
+    post.likedBy = req.body.likedBy;
+    post.save(() => {
+      if (err) return next(err);
+      res.status(200).send(post);
+    });
+  });
 });
 
 // DELETE /users/:username/posts/:id/
 // Route for deleting a specific post
-router.delete('/:username/posts/:id', function(req, res, next) {
-  req.post.remove(function(err) {
-    req.post.save(function(err, post) {
+router.delete('/:username/posts/:id', (req, res, next) => {
+  req.post.remove(() => {
+    req.post.save((err) => {
       if (err) return next(err);
-      res.status(200).send()
+      res.status(200).send();
     });
   });
 });
 
 // POST /users/:username/posts/comment
 // Route for creating a comment for a specific post
-router.post('/:username/posts/:id/comment', function(req, res, next) {
+router.post('/:username/posts/:id/comment', (req, res, next) => {
   if (req.body.comments) { // if we already made the comment and are updating the comments array
-    Post.findById(req.params.id, function(err, post) {
+    Post.findById(req.params.id, (err, post) => {
       if (err) return next(err);
       post.comments = req.body.comments;
-      post.save(function(err, post) {
+      post.save(() => {
         if (err) return next(err);
         res.status(200).json(post);
       });
@@ -251,13 +244,11 @@ router.post('/:username/posts/:id/comment', function(req, res, next) {
     const postedBy = req.body.postedBy;
     const content = req.body.content;
     const comment = new Comment({ postedBy, content });
-    comment.save(function(err, comment) {
+    comment.save((err) => {
       if (err) return next(err);
       res.status(200).json(comment);
     });
   }
-  
-  
 });
 
 // PUT /users/:username/posts/:id/:cid
@@ -281,7 +272,4 @@ router.post('/:username/posts/:id/comment', function(req, res, next) {
 //     commentId: req.params.cid
 //   });
 // });
-
-
-
 module.exports = router;
